d05937c586756aaf4662086637f1f529db1250e3
MYAgaing <nadones2540@ceuandalucia.es>
2024-10-25 17:24:27 +0200

    Update

--- Project3_3.zip_expanded/Projecto 3_3/src/Project3_3/Clase3_3.java
+++ Project3_3.zip_expanded/Projecto 3_3/src/Project3_3/Clase3_3.java
@@ -3,7 +3,7 @@ package Project3_3;
 public class Clase3_3 {
 
 	public static void main(String[] args) {
-		System.out.println("Practica 3_3" + " Nicolás Adones Nuñez");
+		System.out.println("Canela en rama");
 	}
 
 }
